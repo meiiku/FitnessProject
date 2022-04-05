21b44d8e4236591f98814a616314e79b646b0105
Fedor <f.b.fokin@gmail.com>
2022-04-05 17:47:51 +0300

    Initial commit. Imported SnapKit

---
+++ FitnessProject/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FitnessProject
+//
+//  Created by Fedor on 05.04.2022.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
