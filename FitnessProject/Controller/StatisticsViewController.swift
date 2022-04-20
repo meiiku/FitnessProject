26a599b495740647d08f2fc1d2051ae364869d30
Fedor <f.b.fokin@gmail.com>
2022-04-20 18:35:26 +0300

    Got rid of StatisticsView

--- FitnessProject/Controller/StatisticsViewController.swift
+++ FitnessProject/Controller/StatisticsViewController.swift
@@ -6,30 +6,105 @@
 //
 
 import UIKit
+import SnapKit
 
 class StatisticsViewController: UIViewController {
     
-    let statisticsView = StatisticsView()
+    // MARK: - UI Elements
     
-    override func loadView() {
-        self.view = statisticsView
-    }
+    // screen title
+    private lazy var screenTitleLabel: CustomLabel = {
+        let label = CustomLabel(text: "STATISTICS", type: .screenTitle)
+        return label
+    }()
+    
+    // segmented control
+    private let items = ["Неделя", "Месяц"]
+    lazy var segmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.layer.cornerRadius = 9
+        
+        // colors
+        segmentedControl.backgroundColor = Constants.primaryColor
+        segmentedControl.selectedSegmentTintColor = Constants.secondaryColor
+        segmentedControl.setTitleTextAttributes([.foregroundColor : Constants.primaryLabelColor], for: .selected)
+        segmentedControl.setTitleTextAttributes([.foregroundColor : Constants.whiteColor], for: .normal)
+        return segmentedControl
+    }()
+    
+    private lazy var exersicesLabel: CustomLabel = {
+        let label = CustomLabel(text: "Exercises", type: .comment)
+        return label
+    }()
+    
+    // table
+    private let statisticsTable = StatisticsTableView()
+    
+    // MARK: - init
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setViews()
+        setConstraints()
         setMethods()
     }
     
     private func setMethods() {
-        statisticsView.segmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+        segmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
     }
     
     // method for changing segmentedControl tab
     @objc func segmentChanged() {
-        if statisticsView.segmentedControl.selectedSegmentIndex == 0 {
+        if segmentedControl.selectedSegmentIndex == 0 {
             print("Неделя")
         } else {
             print("Месяц")
         }
+    }
+}
+
+// MARK: - setup, constraints
+
+extension StatisticsViewController {
+    
+    private func setViews() {
+        self.view.backgroundColor = Constants.backgroundColor
+        self.view.addSubview(screenTitleLabel)
+        self.view.addSubview(segmentedControl)
+        self.view.addSubview(exersicesLabel)
+        self.view.addSubview(statisticsTable)
+    }
+    
+    private func setConstraints() {
+        
+        // screen title
+        screenTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(75)
+        }
+        
+        // segmented control
+        segmentedControl.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(screenTitleLabel.snp.bottom).offset(31)
+            make.width.equalTo(self.view.layoutMarginsGuide.snp.width)
+        }
+        
+        // exersices control
+        exersicesLabel.snp.makeConstraints { make in
+            make.left.equalTo(self.view.layoutMarginsGuide.snp.left)
+            make.top.equalTo(segmentedControl.snp.bottom).offset(10)
+        }
+        
+        // table
+        statisticsTable.snp.makeConstraints { make in
+            make.top.equalTo(exersicesLabel.snp.bottom).offset(-5)
+            make.left.equalTo(self.view.layoutMarginsGuide.snp.left)
+            make.right.equalTo(self.view.layoutMarginsGuide.snp.right)
+            make.bottom.equalTo(self.view.layoutMarginsGuide.snp.bottom)
+        }
+        
     }
 }
